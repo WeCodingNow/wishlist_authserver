f587718d630b372c3b821e68238ddabf77ac1b6d
nigraforce <aleshka2012@gmail.com>
2019-08-07 22:26:24 +0300

    initial commit

---
+++ settings.py
@@ -0,0 +1,7 @@
+settings = {
+    'secret': '4iig9l2SIc8Ju4ml7VpV',
+    'query_params': ["user_id", "access_token"],
+    'signature_param_name': "sign",
+    'auth_queue_name': "auth_queue",
+    'reg_queue_name': "reg_queue",
+}
